9e99a67e8b74e7c293b0535402314b70be3c43f2
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-09-12 22:07:01 +0300

    More excludes, std.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-clang -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" leak.c -o leak.exe
-clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" leak.cpp -o leak++.exe
+clang -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c99 leak.c -o leak.exe
+clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 -stdlib=libc++ leak.cpp -o leak++.exe
