ad4d0e6b3da4967fbfd3d3e5aadeaf0f021694e0
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-09-12 22:35:09 +0300

    Tryout removing `-stdlib=libc++`.

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ clang -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -st
 clang -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c99 thread.c -o thread.exe
 clang -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c99 good.c -o good.exe
 
-clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 -stdlib=libc++ leak.cpp -o leak++.exe
-clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 -stdlib=libc++ address.cpp -o address++.exe
-clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 -stdlib=libc++ undefined.cpp -o undefined++.exe
-clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 -stdlib=libc++ thread.cpp -o thread++.exe
-clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 -stdlib=libc++ good.cpp -o good++.exe
+clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 leak.cpp -o leak++.exe
+clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 address.cpp -o address++.exe
+clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 undefined.cpp -o undefined++.exe
+clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 thread.cpp -o thread++.exe
+clang++ -O1 -g -fno-sanitize-recover=all -fsanitize-trap=all -fsanitize="${1}" -std=c++20 good.cpp -o good++.exe
