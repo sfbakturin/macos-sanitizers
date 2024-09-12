fba9289840a0c961b2bf9c68b690e6f39c26f1d1
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-09-12 21:37:50 +0300

    Leak example.

---
+++ leak.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+int main(void) {
+  int *not_freed = malloc(sizeof(int) * 4096);
+  not_freed = NULL;
+  return 0;
+}
---
+++ leak.cpp
@@ -0,0 +1,4 @@
+int main() {
+  int *not_freed = new int[4096];
+  not_freed = nullptr;
+}
